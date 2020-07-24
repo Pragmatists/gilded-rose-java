1366085ca854f41dbdd653891063a38ff65bae8a
michal.lipski <kiwaczki@gmail.com>
2020-07-24 14:26:46 +0200

    separated conditions

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -9,57 +9,53 @@ class GildedRose {
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-            updateQuality(i);
+            updateSellIn(items[i]);
 
-            updateSellIn(i);
+            updateQuality(items[i]);
 
-            updateQualityAfterSellin(i);
+            updateQualityAfterSellin(items[i]);
         }
     }
 
-    private void updateQuality(int i) {
-        if (items[i].name.equals("Aged Brie") || items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-            if (items[i].quality < 50) {
-                items[i].quality = items[i].quality + 1;
-
-                if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    if (items[i].sellIn < 11) {
-                        if (items[i].quality < 50) {
-                            items[i].quality = items[i].quality + 1;
-                        }
-                    }
-
-                    if (items[i].sellIn < 6) {
-                        if (items[i].quality < 50) {
-                            items[i].quality = items[i].quality + 1;
-                        }
-                    }
-                }
+    private void updateQuality(Item item) {
+        if (item.name.equals("Aged Brie")) {
+            increaseQuality(item);
+        } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            increaseQuality(item);
+            if (item.sellIn < 11) {
+                increaseQuality(item);
             }
-        } else {
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros") && items[i].quality > 0) {
-                items[i].quality = items[i].quality - 1;
+            if (item.sellIn < 6) {
+                increaseQuality(item);
             }
+        } else if (!item.name.equals("Sulfuras, Hand of Ragnaros"))
+            if (item.quality > 0) {
+                item.quality = item.quality - 1;
+            }
+    }
+
+    private void increaseQuality(Item item) {
+        if (item.quality < 50) {
+            item.quality = item.quality + 1;
         }
     }
 
-    private void updateSellIn(int i) {
-        if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-            items[i].sellIn = items[i].sellIn - 1;
+    private void updateSellIn(Item item) {
+        if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
+            return;
         }
+        item.sellIn = item.sellIn - 1;
     }
 
-    private void updateQualityAfterSellin(int i) {
-        if (items[i].sellIn < 0) {
-            if (items[i].name.equals("Aged Brie")) {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
-                }
-            } else if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                items[i].quality = 0;
-            } else if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                if (items[i].quality > 0)
-                    items[i].quality = items[i].quality - 1;
+    private void updateQualityAfterSellin(Item item) {
+        if (item.sellIn < 0) {
+            if (item.name.equals("Aged Brie")) {
+                increaseQuality(item);
+            } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                item.quality = 0;
+            } else if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                if (item.quality > 0)
+                    item.quality = item.quality - 1;
             }
         }
     }
--- src/test/java/com/gildedrose/TexttestFixture.java
+++ src/test/java/com/gildedrose/TexttestFixture.java
@@ -1,9 +1,12 @@
 package com.gildedrose;
 
-public class TexttestFixture {
-    public static void main(String[] args) {
-        System.out.println("OMGHAI!");
+import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TexttestFixture {
+    @Test
+    void _result_should_be_as_expected() {
         Item[] items = new Item[] {
                 new Item("+5 Dexterity Vest", 10, 20), //
                 new Item("Aged Brie", 2, 0), //
@@ -19,19 +22,39 @@ public class TexttestFixture {
         GildedRose app = new GildedRose(items);
 
         int days = 2;
-        if (args.length > 0) {
-            days = Integer.parseInt(args[0]) + 1;
-        }
-
+        StringBuilder result = new StringBuilder();
         for (int i = 0; i < days; i++) {
-            System.out.println("-------- day " + i + " --------");
-            System.out.println("name, sellIn, quality");
+            result.append("-------- day " + i + " --------").append("\n");;
+            result.append("name, sellIn, quality\n");
             for (Item item : items) {
-                System.out.println(item);
+                result.append(item).append("\n");
             }
-            System.out.println();
             app.updateQuality();
         }
+
+        System.out.println(result);
+        assertEquals("-------- day 0 --------\n" +
+                "name, sellIn, quality\n" +
+                "+5 Dexterity Vest, 10, 20\n" +
+                "Aged Brie, 2, 0\n" +
+                "Elixir of the Mongoose, 5, 7\n" +
+                "Sulfuras, Hand of Ragnaros, 0, 80\n" +
+                "Sulfuras, Hand of Ragnaros, -1, 80\n" +
+                "Backstage passes to a TAFKAL80ETC concert, 15, 20\n" +
+                "Backstage passes to a TAFKAL80ETC concert, 10, 49\n" +
+                "Backstage passes to a TAFKAL80ETC concert, 5, 49\n" +
+                "Conjured Mana Cake, 3, 6\n" +
+                "-------- day 1 --------\n" +
+                "name, sellIn, quality\n" +
+                "+5 Dexterity Vest, 9, 19\n" +
+                "Aged Brie, 1, 1\n" +
+                "Elixir of the Mongoose, 4, 6\n" +
+                "Sulfuras, Hand of Ragnaros, 0, 80\n" +
+                "Sulfuras, Hand of Ragnaros, -1, 80\n" +
+                "Backstage passes to a TAFKAL80ETC concert, 14, 21\n" +
+                "Backstage passes to a TAFKAL80ETC concert, 9, 50\n" +
+                "Backstage passes to a TAFKAL80ETC concert, 4, 50\n" +
+                "Conjured Mana Cake, 2, 5\n", result.toString());
     }
 
 }
