f2cb3a5762845a2be1f34adf8dd2c8e5ca6370b6
michal.lipski <kiwaczki@gmail.com>
2020-09-15 15:29:50 +0200

    delegation to item updater

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -9,50 +9,9 @@ class GildedRose {
 
     public void updateQuality() {
         for (Item item : items) {
-            updateOneItem(item);
+            ItemUpdater itemUpdater = new ItemUpdater();
+            itemUpdater.updateQuality(item);
+            itemUpdater.updateAfterExpiration(item);
         }
     }
-
-    private void updateOneItem(Item item) {
-        switch (item.name) {
-            case "Aged Brie":
-                item.increaseQuality();
-                item.reduceSellIn();
-                break;
-            case "Backstage passes to a TAFKAL80ETC concert":
-                item.increaseQuality();
-                if (item.sellIn < 11) {
-                    item.increaseQuality();
-                }
-                if (item.sellIn < 6) {
-                    item.increaseQuality();
-                }
-                item.reduceSellIn();
-                break;
-            case "Sulfuras, Hand of Ragnaros":
-                break;
-            default:
-                item.reduceSellIn();
-                item.reduceQuality();
-                break;
-        }
-
-
-        if (item.sellIn < 0) {
-            switch (item.name) {
-                case "Aged Brie":
-                    item.increaseQuality();
-                    break;
-                case "Backstage passes to a TAFKAL80ETC concert":
-                    item.quality = 0;
-                    break;
-                case "Sulfuras, Hand of Ragnaros":
-                    return;
-                default:
-                    item.reduceQuality();
-                    break;
-            }
-        }
-    }
-
 }
